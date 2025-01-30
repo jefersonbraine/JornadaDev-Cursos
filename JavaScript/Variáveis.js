eacc99b54e3cbf63e2b97bbabd2fc4132d4e3893
Jeferson <jefeleal363@gmail.com>
2025-01-30 16:05:53 -0300

    feat: att

---
+++ JavaScript/Variáveis.js
@@ -0,0 +1,41 @@
+// COMO DECLARAR UMA VARIÁVEL
+let nomeDaVariavel = 'João';
+console.log(`Olá ${nomeDaVariavel}`);
+
+//TIPOS DE DADOS EM VARIAVEIS
+let number = 10;
+let string = 'João';
+let verdadeiro = true;
+let falso = false;
+
+
+// floats
+
+let contaBancaria = 765.90
+
+// arrays
+
+let frutas = ["Maça", "Uva", "Laranja"];
+
+
+//objetos
+
+let xicara = {
+    cor: "vermelha",
+    tamanho: "grande",
+    peso: 250,
+    material: "porcelana"
+}
+
+// Acessando uma propriedade de um objeto
+
+xicara.peso
+
+
+let carroUno = {
+    modelo: "Uno mille",
+    cores: ['Branco', 'Preto'],
+    ano: 1984,
+    marca: 'Fiat',
+    foraLinha: true,
+}
