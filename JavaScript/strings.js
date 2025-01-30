f298f2111ce3180b7466609cd364e3af0c7f9d69
Jeferson <jefeleal363@gmail.com>
2025-01-30 16:12:14 -0300

    feat: att

---
+++ JavaScript/strings.js
@@ -0,0 +1,15 @@
+
+let string = 'ola mundo';
+
+// concatenar
+
+let ms1 = 'ola';
+let ms2 = ' mundo';
+let concatenar = ms1 + ms2;
+console.log(concatenar);
+
+
+let usuario = prompt('Qual o seu nome?');
+
+let saudacao = `olá ${usuario}`;
+console.log(saudacao);
