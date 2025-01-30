ac13ba7f4ab1fd6062a9c8a702f80db6caab5ac0
Jeferson <jefeleal363@gmail.com>
2025-01-30 16:31:37 -0300

    feat: att

---
+++ JavaScript/arrays.js
@@ -0,0 +1,17 @@
+let cargos = ['Administrador', 'Vendedor', 'Atendente', 'Advogado', 'Programador', 'Médico'];
+
+cargos[1] = 'Engenheiro';
+
+cargos.push('Estudante');
+cargos.pop();
+
+console.log(cargos);
+
+// Exercicio
+
+let pedro = 40;
+let amigos = 5;
+
+let dividir = 40 / 5;
+
+console.log(dividir);
